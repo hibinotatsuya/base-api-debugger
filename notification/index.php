201cddad29d8a5c8f7a758f6864a14daf938ffd9
hibinotatsuya <hibinotatsuya@gmail.com>
2015-12-25 19:14:26 +0900

    add notification

---
+++ notification/index.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+
+$post = $_POST;
+$get = $_GET;
+
+$data = "";
+
+$data .= date("Y-m-d H:i:s") . "\n";
+
+$data .= "POST\n";
+foreach ($post as $key => $value) {
+	$data .= $key . "=" . $value . "\n";
+}
+
+$data .= "GET\n";
+foreach ($get as $key => $value) {
+	$data .= $key . "=" . $value . "\n";
+}
+
+$data .= "\n";
+
+file_put_contents("../tmp/log.txt", $data, FILE_APPEND);
+
+echo "finish!!";
