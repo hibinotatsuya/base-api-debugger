7fbcd491cf442b62886f3697c61ec8ef14ae7159
hibinotatsuya <hibinotatsuya@gmail.com>
2015-12-25 19:12:40 +0900

    add config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,8 @@
+<?php
+// config.phpにリネームする
+define('API_HOST', 'https://api.thebase.in');
+define('API_VERSION', '1');
+define('CLIENT_ID', '');
+define('CLIENT_SECRET', '');
+define('REDIRECT_URI', '');
+
