93d04e846fff1f8c0e16e51d45c26cf95eb463b8
hibinotatsuya <hibinotatsuya@gmail.com>
2016-01-05 19:50:49 +0900

    add categories.php

---
+++ categories.php
@@ -0,0 +1,44 @@
+<?php
+// カテゴリー情報の一覧を取得
+require_once("config.php");
+
+$token = (isset($_POST['token'])) ? $_POST['token'] : NULL;
+if (empty($token) && isset($_COOKIE['base_access_token'])) {
+	$token = $_COOKIE['base_access_token'];
+}
+
+$headers = array(
+	'Authorization: Bearer ' . $token,
+);
+$request_options = array(
+	'http' => array(
+		'method'  => 'GET',
+		'header'  => implode("\r\n", $headers),
+		'ignore_errors' => true,
+	),
+);
+$context = stream_context_create($request_options);
+
+$response_body = '';
+$parsed_response = '';
+if (!empty($_POST)) {
+	$response_body = file_get_contents(API_HOST . '/' . API_VERSION . '/categories', false, $context);
+	$parsed_response = json_decode($response_body, true);
+}
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>BASE API Debugger</title>
+</head>
+<body>
+	<h1><a href="index.php">BASE API Debugger</a></h1>
+	<h2>GET /<?php echo API_VERSION; ?>/categories</h2>
+	<form method="POST" action="categories.php">
+		access_token <input type="text" name="token" value="<?php echo $token; ?>" style="width:300px"><br>
+		<input type="submit" value="submit" name="submit">
+	</form>
+	<textarea style="width:600px;height:200px;"><?php echo $response_body; ?></textarea>
+	<pre><?php var_dump($parsed_response); ?></pre>
+</body>
+</html>
