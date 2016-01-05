16d4d1bb0fcf3ac869521aec6df7a2028c5d9724
hibinotatsuya <hibinotatsuya@gmail.com>
2016-01-05 19:44:13 +0900

    add items_detail.php

--- items_detail.php
+++ items_detail.php
@@ -1,12 +1,12 @@
 <?php
-// 注文情報を取得
+// 商品情報を取得
 require_once("config.php");
 
 $token = (isset($_POST['token'])) ? $_POST['token'] : NULL;
 if (empty($token) && isset($_COOKIE['base_access_token'])) {
 	$token = $_COOKIE['base_access_token'];
 }
-$unique_key = (isset($_POST['unique_key'])) ? $_POST['unique_key'] : NULL;
+$item_id = (isset($_POST['item_id'])) ? $_POST['item_id'] : NULL;
 
 $headers = array(
 	'Authorization: Bearer ' . $token,
@@ -23,7 +23,7 @@ $context = stream_context_create($request_options);
 $response_body = '';
 $parsed_response = '';
 if (!empty($_POST)) {
-	$response_body = file_get_contents(API_HOST . '/' . API_VERSION . '/orders/detail/' . $unique_key, false, $context);
+	$response_body = file_get_contents(API_HOST . '/' . API_VERSION . '/items/detail/' . $item_id, false, $context);
 	$parsed_response = json_decode($response_body, true);
 }
 ?>
@@ -34,10 +34,10 @@ if (!empty($_POST)) {
 </head>
 <body>
 	<h1><a href="index.php">BASE API Debugger</a></h1>
-	<h2>GET /<?php echo API_VERSION; ?>/orders/detail/:unique_key</h2>
-	<form method="POST" action="orders_detail.php">
+	<h2>GET /<?php echo API_VERSION; ?>/items/detail/:item_id</h2>
+	<form method="POST" action="items_detail.php">
 		access_token <input type="text" name="token" value="<?php echo $token; ?>" style="width:300px"><br>
-		unique_key <input type="text" name="unique_key" value="<?php echo $unique_key; ?>" style="width:300px"><br>
+		item_id <input type="text" name="item_id" value="<?php echo $item_id; ?>" style="width:300px"><br>
 		<input type="submit" value="submit" name="submit">
 	</form>
 	<textarea style="width:600px;height:200px;"><?php echo $response_body; ?></textarea>
