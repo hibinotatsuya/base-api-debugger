e58fdd712cb0ef512683b558e64fc5b944817e60
hibinotatsuya <hibinotatsuya@gmail.com>
2017-08-25 15:54:18 +0900

    items.php edit

--- items.php
+++ items.php
@@ -10,12 +10,16 @@ $order = (isset($_POST['order'])) ? $_POST['order'] : 'list_order';
 $sort = (isset($_POST['sort'])) ? $_POST['sort'] : 'asc';
 $limit = (isset($_POST['limit'])) ? $_POST['limit'] : 10;
 $offset = (isset($_POST['offset'])) ? $_POST['offset'] : 0;
+$max_image_no = (isset($_POST['max_image_no'])) ? $_POST['max_image_no'] : 5;
+$image_size = (isset($_POST['image_size'])) ? $_POST['image_size'] : 'origin';
 
 $params = array(
 	'order' => $order,
 	'sort' => $sort,
 	'limit'  => $limit,
 	'offset' => $offset,
+	'max_image_no' => $max_image_no,
+	'image_size' => $image_size,
 );
 $headers = array(
 	'Authorization: Bearer ' . $token,
@@ -54,6 +58,8 @@ if (!empty($_POST)) {
 		sort <input type="text" name="sort" value="<?php echo $sort; ?>" style="width:300px"><br>
 		limit <input type="text" name="limit" value="<?php echo $limit; ?>" style="width:300px"><br>
 		offset <input type="text" name="offset" value="<?php echo $offset; ?>" style="width:300px"><br>
+		max_image_no <input type="text" name="max_image_no" value="<?php echo $max_image_no; ?>" style="width:300px"><br>
+		image_size <input type="text" name="image_size" value="<?php echo $image_size; ?>" style="width:300px"><br>
 		<input type="submit" value="submit" name="submit">
 	</form>
 	<textarea style="width:600px;height:200px;"><?php echo $response_body; ?></textarea>
